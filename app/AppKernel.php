a39fd6ae02c63ce138672332677f37263748b042
Joe Robles <joe@colldev.com>
2013-01-19 23:45:35 -0500

    CollDevMainBundle creado

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,8 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
+//            new FOS\UserBundle\FOSUserBundle(),
+            new CollDev\MainBundle\CollDevMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
